48a07980031ae447982b09c3745869b8f2b22d97
KLaZ <muhabdanulikhlas0983@gmail.com>
2024-02-10 18:09:34 +0700

    membuat halaman portfolio detail

--- client/src/components/Fragments/CardPortfolio.jsx
+++ client/src/components/Fragments/CardPortfolio.jsx
@@ -1,10 +1,10 @@
 import {Link} from 'react-router-dom';
 
 
-const CardPortfolio = ({id, image, title, subtitle, description}) => {
+const CardPortfolio = ({id, imageCard, title, subtitle, description}) => {
   return (
     <div className="max-w-[267px] mx-auto mt-[43px] rounded-lg font-body overflow-hidden shadow-lg box-border">
-      <img src={image} alt="Cards" className="w-full h-auto" />
+      <img src={imageCard} alt="Cards" className="w-full h-auto" />
       <div className="px-[14px] py-3 box-border leading-3">
         <h2 className="text-xs ">{subtitle}</h2>
         <h1 className="text-[16px] font-bold text-left my-[11px]">{title}</h1>
--- client/src/pages/portfolioDetail.jsx
+++ client/src/pages/portfolioDetail.jsx
@@ -16,19 +16,47 @@ const PortfolioDetail = () => {
       });
     })
     
+    // Filter portofolio berdasarkan id yang sesuai
+    const filteredPortfolios = portfolios.filter(portfolio => portfolio.id === parseInt(id));
+
     return (
         <>
           <NavBar />
-          <main className="px-2 mb-14 box-border">
-            <h1 className="mt-[80px] text-2xl font-bold text-center">Portfolio</h1>
-            <p className="mt-[30px] w-full text-xs font-body leading-4 text-black px-[12px] text-center">
-              testing portfolio detail
-            </p> 
-            <h2>Portfolio Detail for ID: {id}</h2>
-            {portfolios.map((portfolio)=>(
-              <CardPortfolio key={portfolio.id} {...portfolio}/>
+            {/* Menampilkan data object portfolios*/}
+            {filteredPortfolios.map(portfolio => (
+            <main key={portfolio.id} className={`bg-white mt-14 pb-20 box-border border border-white font-body`}>
+              {/* top background */}
+              <div className={portfolio.backgroundColor}>
+                <div className={` h-48 w-full`}></div>
+              </div>
+              {/* bagian mockup */}
+              <section className="absolute z-30 inset-x-0 top-[15%]">
+                <img src={portfolio.mockup} alt="Mockup Portfolio" className="mx-auto"/>
+                {/* under shadow blur */}
+                <div className="w-[211px] h-[20px] mx-auto bg-gradient-to-r from-white via-black to-white blur-[12px] rounded-full "></div>
+              </section>
+              {/* border radius div */}
+              <div className="absolute z-20 top-52 w-full h-20 bg-white rounded-customRounded"></div>
+              {/* Deskripsi */}
+              <section className="mt-1 pt-24 mb-9 bg-white px-[15px]">
+                <h2 className="text-sm text-primary">{portfolio.subtitle}</h2>
+                <h1 className="mt-2 font-bold text-lg">{portfolio.title}</h1>
+                <p className="mt-4 text-xs">{portfolio.descriptionDetail}</p>
+              </section>
+              {/* teknologi */}
+              <section className="bg-[#F4F4F4] p-4">
+                <h1 className="font-bold text-base">Teknologi</h1>
+                <div className="flex gap-2 mt-3">
+                  {portfolio.teknologi.map(tech => (
+                    <div key={tech.title}>
+                      {/* <img src="../images/react.png" alt={tech.title} /> */}
+                      <img src={tech.image} alt={tech.title} />
+                    </div>
+                  ))}
+                </div>
+              </section>
+            </main>
             ))}
-          </main>
           <Footer />
         </>
     );
