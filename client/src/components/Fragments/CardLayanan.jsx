fcb0205cb753d89fc643b9ace9f059b51cb75eb6
KLaZ <muhabdanulikhlas0983@gmail.com>
2024-04-05 23:05:41 +0700

    update tampilan halaman layanan sesuai desain terbaru

--- client/src/components/Fragments/CardLayanan.jsx
+++ client/src/components/Fragments/CardLayanan.jsx
@@ -1,22 +1,23 @@
 import PropTypes from "prop-types";
 
-const CardLayanan = ({ image, title, content }) => {
 
+const CardLayanan = ({ image, title, content }) => {
   CardLayanan.propTypes = {
     image: PropTypes.string,
     title: PropTypes.string,
-    content: PropTypes.string
-  }
+    content: PropTypes.string,
+  };
 
   return (
-    <div className="relative max-w-[267px] h-[290px] mx-auto mt-[43px] rounded-[10px] font-body overflow-hidden shadow-lg box-border">
+    <div className="relative max-w-[267px] h-[290px] mx-auto mt-[43px] rounded-[10px] font-body overflow-hidden shadow-shadowCardLayanan box-border">
       <img src={image} alt="Cards" className="w-[57px] mx-auto mt-[26px]" />
-      <h1 className="text-[16px] font-bold text-center mb-5 mt-[14px]">{title}</h1>
-      <p className="word-wrap px-[13px] text-center text-xs">{content}</p>
+      <h1 className="text-[16px] font-bold text-center mb-5 mt-[14px]">
+        {title}
+      </h1>
+      <p className="word-wrap px-[13px] text-justify text-xs">{content}</p>
       <div className="absolute bottom-0 w-full border border-[#FFB10A]"></div>
     </div>
   );
 };
-
 
 export default CardLayanan;
